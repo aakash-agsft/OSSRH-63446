4f848a9a2173d7def8a536408c64d4cef218bdb7
aakashr <aakashr@agsft.com>
2021-01-15 18:36:29 +0530

    Projecrt added

---
+++ src/main/java/com/agsft/publishjar/ArithmeticOperation.java
@@ -0,0 +1,19 @@
+package com.agsft.publishjar;
+
+public class ArithmeticOperation {
+    public static Double add(Double a, Double b) {
+        return a + b;
+    }
+
+    public static Double subtract(Double a, Double b) {
+        return a - b;
+    }
+
+    public static Double divide(Double a, Double b) {
+        return a / b;
+    }
+
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/com/agsft/publishjar/PublishjarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agsft.publishjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishjarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
