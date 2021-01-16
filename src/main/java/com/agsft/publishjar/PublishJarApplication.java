b5644afa40f069cf828e68eb101095dd95694976
aakashr <aakashr@agsft.com>
2021-01-16 20:18:04 +0530

    a

---
+++ src/main/java/com/agsft/publishjar/PublishJarApplication.java
@@ -0,0 +1,11 @@
+package com.agsft.publishjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishJarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PublishJarApplication.class, args);
+    }
+}
